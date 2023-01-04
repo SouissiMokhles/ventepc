5078998698c55d048fb300b9b87745bbdd8e0797
Souissi Mokhles <mokhlessouissi@gmail.com>
2023-01-04 13:34:07 +0100

    133304012023

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button class="btn btn-success">Se connecter</button>
+    <p>Vous n'avez pas un compte? <a href="#">Crée un compte</a></p>
   </div>
 </template>
 
